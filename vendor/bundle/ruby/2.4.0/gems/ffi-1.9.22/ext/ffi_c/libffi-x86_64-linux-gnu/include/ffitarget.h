814c0631948154ff2b9fb742dbc64200094f6d2a
Amol Khiste <amol.khiste@gmail.com>
2018-02-25 10:47:17 +0530

    Remove vendor/bundle
    Installed vendor gems in system instead, as some gems won't build on
    GitHub.

--- vendor/bundle/ruby/2.4.0/gems/ffi-1.9.22/ext/ffi_c/libffi-x86_64-linux-gnu/include/ffitarget.h
+++ vendor/bundle/ruby/2.4.0/gems/ffi-1.9.22/ext/ffi_c/libffi-x86_64-linux-gnu/include/ffitarget.h
@@ -1,1 +1,1 @@
-/home/amolbk/jekyll/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.22/ext/ffi_c/libffi/src/x86/ffitarget.h
+/home/amolbk/projects/pinnacparijaat/pinnacparijaat.github.io/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.22/ext/ffi_c/libffi/src/x86/ffitarget.h
